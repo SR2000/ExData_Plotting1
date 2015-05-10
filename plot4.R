23122a4c9f06789b8ba696ba926d5e4ff30d7fb0
SR2000 <galaxyblaster@gmail.com>
2015-05-09 22:23:33 -0400

    changed comment

--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@ table_orig<-read.table("ds4/household_power_consumption.txt", header=TRUE,sep=";
 table_orig$newdate <- with(table_orig, as.POSIXct(paste(table_orig$Date, table_orig$Time), format="%d/%m/%Y %H:%M:%S"))
 table_req<-subset(table_orig, (table_orig$newdate)>='2007-02-01 00:00:00' & (table_orig$newdate)<='2007-02-02 23:59:00')
 
-#Plotting the 4 graphs in the clockwise direction
+#Plotting the 4 graphs
 png("plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
 plot(table_req$Global_active_power ~ table_req$newdate, table_req, type = "l", xlab="", ylab="Global Active Power")
